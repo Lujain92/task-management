1a58e89bb12e54fc6cae4bfb8533f0e57591b19c
Lujain92 <lujain.aljarah@cds-global.com>
2023-12-20 14:17:02 +0300

    add error controller

---
+++ controllers/error.js
@@ -0,0 +1,8 @@
+/**
+ * render 404 page when no page available.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
+exports.get404 = (req, res) => {
+    res.status(404).render('404');
+  };
